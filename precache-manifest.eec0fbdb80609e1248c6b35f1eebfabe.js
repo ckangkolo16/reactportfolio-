9789ae69f8574d9b2e8f5535e63f5c0d63f9210f
Chobu Nkolo-Kangkolo <ckangkolo16@yahoo.com>
2020-08-17 21:27:25 -0500

    Updates

---
+++ precache-manifest.eec0fbdb80609e1248c6b35f1eebfabe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ee295d863aa9cc91e71f41c8c8385e4",
+    "url": "/reactportfolio-/index.html"
+  },
+  {
+    "revision": "d526d0491d9bb51acb92",
+    "url": "/reactportfolio-/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "6dc898d98a43b897d7c9",
+    "url": "/reactportfolio-/static/css/main.7baca40e.chunk.css"
+  },
+  {
+    "revision": "d526d0491d9bb51acb92",
+    "url": "/reactportfolio-/static/js/2.5ff2488b.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/reactportfolio-/static/js/2.5ff2488b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dc898d98a43b897d7c9",
+    "url": "/reactportfolio-/static/js/main.00b7286f.chunk.js"
+  },
+  {
+    "revision": "d97f1d6102d3b85f3b71",
+    "url": "/reactportfolio-/static/js/runtime-main.3c854c62.js"
+  }
+]);
